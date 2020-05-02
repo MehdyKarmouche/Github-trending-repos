c8a6a858aa893abb31210ca9c23e7f00821609a3
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-05-03 01:09:37 +0200

    Get different languages
    Get the different languages of the 100 repos

--- routes/repos.js
+++ routes/repos.js
@@ -3,10 +3,12 @@ var router = express.Router();
 var axios = require('axios');
 
 /* GET home page. */
-router.get('/repos', async function(req, res, next) {
-  const repos = await getRepos();
+router.get('/languages', async function(req, res, next) {
+  const repos = await diffLangs();
   res.json(repos);
 });
+
+
 
 
 //fetch repos along with their language
@@ -31,6 +33,26 @@ async function  getRepos(){
   }
   return repos;
 }
+
+//function to remove duplicate languages
+async function diffLangs(){
+  const repos = await getRepos();
+
+  var diffLangs = [];
+  var l = repos.length;
+  var index=0;
+
+  for(var i=0; i<l; i++){
+    if(!diffLangs.includes(repos[i].language)){
+      diffLangs[index] = repos[i].language;
+      index++;
+    }
+  }
+  
+  return diffLangs;
+
+}
+
 
 
 module.exports = router;
