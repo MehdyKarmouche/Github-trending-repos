3e23c38c6d5f9276bb2a692a8e55d0bf98adad83
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-05-02 17:58:57 +0200

    Get all repos
    -Get 100 repos using Github API
    -Get languages and name of those repos

--- routes/repos.js
+++ routes/repos.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("Test")
+router.get('/repos', async function(req, res, next) {
+  const repos = await getRepos();
+  res.json(repos);
 });
+
+
+//fetch repos along with their language
+async function  getRepos(){
+  // YYYY-MM-DD
+  var date = new Date();
+  date.setDate(date.getDate() -1);
+  var dateString = date.toISOString().split('T')[0];
+
+  const url = "https://api.github.com/search/repositories?q=created:>"+dateString+"&sort=stars&order=desc";
+  let response = await axios.get(url); 
+  var l = response.data.items.length;
+  var repos =[];
+
+  for(var i = 0; i<l; i++){
+    var element = {}
+    if(!repos.includes(response.data.items[i].language)){
+      element['language'] = response.data.items[i].language;
+      element['nameRepo'] = response.data.items[i].name;
+      repos.push(element);
+    }
+  }
+  return repos;
+}
+
 
 module.exports = router;
